2d75773603381143df5deb7110df71de5588a97b
Jeff Haynie <jhaynie@appcelerator.com>
2014-12-28 15:47:29 -0800

    - don't reinstall on setup unless --force - run setup inside this cli vs. appc - properly sort versions - better error message for invalid shasum

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-module.exports = require('./lib');
+var fs = require('fs'),
+	path = require('path'),
+	util = require('./lib/util'),
+	bin = util.getInstallBinary();
+
+if (!bin) {
+	throw new Error("you must run appc setup before you can require this module");
+}
+
+// export our module impersonating the module we're delegating to
+var pkgdir = path.join(path.dirname(bin),'..');
+module.exports = require(pkgdir);
